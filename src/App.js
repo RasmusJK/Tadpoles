e0f5b159e9760d51ccbd901957b59fa0be4b1e40
RasmusJK <rasmus.karling@gmail.com>
2021-10-01 11:37:57 +0300

    added map and markers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import{MapContainer,TileLayer,Popup,Marker} from "react-leaflet";
+import useSwr from "swr";
+import {useEffect, useState} from "react";
+
+//const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 function App() {
-  return (
+    //const url = "https://open-api.myhelsinki.fi/v1/places/?tags_search=Beach"
+ //   const {data,error} = useSwr(url,{fetcher})
+
+    //let beaches = data && !error ? data : [] ;
+
+
+    //
+    const [beach,setBeach]= useState([]);
+useEffect(()=> {
+  getBeaches().then();
+    console.log("rarara",beach);
+},[]);
+
+    const getBeaches = async ()=>{
+        const response = await fetch('https://open-api.myhelsinki.fi/v1/places/?tags_search=Beach');
+        const data = await response.json();
+        setBeach(data.data);
+
+    };
+////
+
+    return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <MapContainer center={[60.169857,24.938379]} zoom={12}>
+       <TileLayer
+    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    />
+       {beach.map(b =>(
+           <Marker
+           key={b.id}
+           position={[b.location.lat,b.location.lon]}
+           />
+       ))}
+   </MapContainer>
     </div>
   );
 }
