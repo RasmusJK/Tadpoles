3d3e0f978c6a79e341233ee20bb149642bbf7b14
RasmusJK <rasmus.karling@gmail.com>
2021-10-07 15:52:49 +0300

    added drawer menu on top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,13 +5,32 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import {useState} from "react";
+import {Button, Drawer} from "@mui/material";
+import  {makeStyles} from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+
+    drawer: {
+        height:"100%",
+        width: "25vh",
+        display:"flex",
+        flexDirection:"column"
+    }
+}));
 
 const TopBar=()=> {
+    const  [open, setOpen]= useState(false);
+    const handleDrawer = () =>{
+        setOpen(true);
+    }
+    const classes = useStyles();
     return (
+        <div>
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
+                    <IconButton onClick={handleDrawer}
                         size="large"
                         edge="start"
                         color="inherit"
@@ -27,6 +46,23 @@ const TopBar=()=> {
                 </Toolbar>
             </AppBar>
         </Box>
+            <Drawer
+                anchor="left"
+                open={open}
+                onClose={() => {
+                    setOpen(false);
+                    }}>
+                <div className={classes.drawer}>
+
+
+                    <Button >Login</Button>
+
+
+
+                </div>
+            </Drawer>
+        </div>
+
     );
 }
 export default TopBar;
