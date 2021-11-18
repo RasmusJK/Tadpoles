ad38193e05324ed67ee03acbf39dc3a1be4f396a
RasmusJK <rasmus.karling@gmail.com>
2021-11-18 20:22:22 +0200

    added water and air temp, changed to axios for fetching

--- src/components/Beach.js
+++ src/components/Beach.js
@@ -32,22 +32,77 @@ const Beach = ({match}) => {
     const [address, setAddress] = useState("");
     const [description, setDescription] = useState("");
     const [image, setImage] =useState("");
-    console.log("ID", Id);
+    const [water, setWater] = useState("Dataa ei ole saatavilla");
+    const [temp, setTemp] = useState("Dataa ei ole saatavilla");
+    let sensor = "E703";
+
     const [loading, setLoading] =useState(false);
     const [error,setError] = useState(false);
 
+const fetchData = () =>{
+try {
+    if (Id === 1920 ){
+        sensor ="1A97";
+    } else if (Id === 978){
+        sensor= "1AB9";
+    } else if (Id === 2962) {
+        sensor="1ADA";
+    }else if (Id === 3738) {
+        sensor="1BA6";
+    } else if (Id === 4037) {
+        sensor="1BBE";
+    }else if (Id === 1208) {
+        sensor="4d86";
+    }else if (Id === 2939) {
+        sensor="4E0E";
+    }else if (Id === 143) {
+        sensor="4FB9";
+    }
+    const beachData =axios.get(`https://open-api.myhelsinki.fi/v1/place/${Id}`);
+    console.log("BeachDATA",beachData)
+    const weatherData = axios.get(`https://iot.fvh.fi/opendata/uiras/70B3D5705000${sensor}_v1.json`);
+    axios.all([beachData,weatherData]).then(
+        axios.spread((...allData)=>{
+            const allDataBeach = allData[0]
+            const allWeather = allData[1]
+            console.log("axios1",allDataBeach.data)
+            console.log("axios2",allWeather.data.data[allWeather.data.data.length-1])
+            if (sensor !== "E703"){
+                setWater(allWeather.data.data[allWeather.data.data.length-1].temp_water)
+                setTemp(allWeather.data.data[allWeather.data.data.length-1].temp_air)
+            }
 
+            setBeachId(allDataBeach.data.id);
+            setBeachNameEn(allDataBeach.data.name.fi);
+            setAddress(allDataBeach.data.location.address.street_address);
+            setDescription(allDataBeach.data.description.body);
+
+        })
+    )
+
+}catch (e) {
+    console.log("error", e);
+}
+
+}
 
     useEffect( () => {
         async function getBeach() {
             try {
                 setLoading(true)
+                fetchData();
+                /*
                 const response = await fetch(`https://open-api.myhelsinki.fi/v1/place/${Id}`);
+                const beachResponse = await fetch(`https://iot.fvh.fi/opendata/uiras/70B3D57050004FB9_v1.json`);
                 const data = await response.json();
+                console.log("weatherdata",data)
                 setBeachId(data.id);
-                setBeachNameEn(data.name.en);
+                setBeachNameEn(data.name.fi);
                 setAddress(data.location.address.street_address);
                 setDescription(data.description.body);
+*/
+
+
 
             } catch (error) {
                 console.log(error);
@@ -92,6 +147,8 @@ const Beach = ({match}) => {
                     <p>{beachNameEn}</p>
                     <p>{address}</p>
                     <p>{description}</p>
+                    <p>Water temp: {water}°C</p>
+                    <p>Air temp: {temp}°C</p>
                 </div>
             ) }
             </div>
