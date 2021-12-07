a732c47b6a1e7303d1cae3eda073893cf1407d21
RasmusJK <rasmus.karling@gmail.com>
2021-12-07 16:50:48 +0200

    display firestore data on the app

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,7 +1,19 @@
 import '../App.css';
 import React from "react";
 import {useState, useEffect} from "react";
-import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import {
+    Button,
+    Card,
+    Divider,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Rating,
+    Select,
+    TextField
+} from "@mui/material";
 import { collection, addDoc, query, getDocs, where } from "firebase/firestore";
 import {db, auth} from "../Utils/firebase";
 import {useAuthState} from 'react-firebase-hooks/auth';
@@ -12,6 +24,7 @@ const Feedback = (id) =>{
     const [feedback, setFeedback] = useState("");
     const [user, loading, error] = useAuthState(auth);
     const [review, setReview] = useState(1);
+    const [dbData, setDbData] = useState([]);
 
     const sendData =async () => {
         try {
@@ -19,7 +32,7 @@ const Feedback = (id) =>{
                 id: id.id,
                 review: review,
                 feedback: feedback,
-
+                uName : auth.currentUser.displayName
 
             });
             console.log("Document written with ID: ", docRef.id);
@@ -35,41 +48,64 @@ const Feedback = (id) =>{
 
 // Create a query against the collection.
        const q = query(reviewsRef, where("id", "==", id.id));
+      const data = [];
        const querySnapshot = await getDocs(q);
        querySnapshot.forEach((doc) => {
            // doc.data() is never undefined for query doc snapshots
-           console.log(doc.id, " => ", doc.data());
-       });
+           console.log(doc.id, " => ", doc.data().review);
+           data.push({review: doc.data().review, feedback: doc.data().feedback, uName: doc.data().uName})
+       }
+       );
+       setDbData(data);
+
    },[])
 
     return (
        <div>
-           <TextField id="outlined-basic" label="Feedback" variant="outlined"
+           <TextField sx={{
+               margin: "auto",
+               width: "75%",
+              height: "40%"
+           }} id="outlined-basic" label="Feedback" variant="outlined" multiline
+                      maxRows={3}
                       onChange={(e => setFeedback(e.target.value))}/>
 
-           <Box sx={{ minWidth: 120 }}>
-               <FormControl fullWidth>
-                   <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                   <Select
-                       labelId="demo-simple-select-label"
-                       id="demo-simple-select"
-                       value={review}
-                       label="Review"
-                       onChange={(e => setReview(e.target.value))}
-                   >
-                       <MenuItem value={1}>1</MenuItem>
-                       <MenuItem value={2}>2</MenuItem>
-                       <MenuItem value={3}>3</MenuItem>
-                       <MenuItem value={4}>4</MenuItem>
-                       <MenuItem value={5}>5</MenuItem>
-                   </Select>
-               </FormControl>
-           </Box>
+           <Rating
+               name="simple-controlled"
+               value={review}
+               onChange={(event, newValue) => {
+                   setReview(newValue);
+               }}
+           />
+           {user ? <Button variant="contained" onClick={sendData}>Submit review</Button>: <Button disabled variant="contained" onClick={sendData}>Log in to post review</Button>  }
 
-           <Button variant="contained" onClick={sendData}>Submit review</Button>
-           <ul>
 
-           </ul>
+           <Paper style={{ padding: "10px 20px" , overflow: "auto", maxHeight: "300px" , marginTop: "10px"}}>
+               {dbData.map((data, index) => {
+                   return (
+                            <div key={index}>
+                           <Grid  container wrap="nowrap" spacing={2}>
+
+                               <Grid justifyContent="left" item xs zeroMinWidth>
+                                   <h4 style={{ margin: 0, textAlign: "left" }}>{data?.uName}</h4>
+                                   <p style={{ textAlign: "left", overflowY:"auto" }}>
+                                       {data?.feedback}
+                                   </p>
+
+                                   <Rating sx={{ float: "left"}} name="read-only" value={data?.review} readOnly />
+                               </Grid>
+
+                           </Grid>
+                                <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
+                            </div>
+
+
+
+                   );
+               })}
+
+
+           </Paper>
 
        </div>
     )
